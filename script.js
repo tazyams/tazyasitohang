bc87d20012073c43d52d1234cc1e212c636136e8
tazyams <sitohangtazya@gmail.com>
2025-03-19 00:08:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -25,25 +25,6 @@ searchInput.addEventListener('input', () => {
   });
 });
 
-const bookList = document.getElementById('bookList');
-
-function addBook(title, author, description, imageUrl) {
-    const bookDiv = document.createElement('div');
-    bookDiv.className = 'buku';
-    
-    bookDiv.innerHTML = `
-        <h3>${title}</h3>
-        <p>${author}</p>
-        <img src="${imageUrl}" alt="${title}" style="width:100px;height:auto;">
-        <p>${description}</p>
-        <button class="baca-sekarang">Baca Sekarang</button>
-    `;
-
-    bookList.appendChild(bookDiv);
-}
-
-addBook("Atomic Habits", "James Clear", "Buku ini mengupas cara-cara untuk membentuk kebiasaan baik dan menghilangkan kebiasaan buruk melalui perubahan kecil yang konsisten. James Clear menjelaskan bahwa kebiasaan adalah fondasi dari perubahan yang signifikan dalam hidup. Melalui pendekatan yang berbasis pada ilmu psikologi dan neuroscience, Clear memperkenalkan The Four Laws of Behavior Change yang membantu pembaca membentuk pola hidup yang lebih produktif. Dengan memberikan contoh dari berbagai tokoh sukses, buku ini menjadi panduan praktis bagi siapa saja yang ingin meningkatkan kualitas hidup melalui kebiasaan yang positif.", "https://www.gramedia.com/blog/content/images/2021/07/1--2--1.jpg");
-addBook("Filosofi Teras", "Henry Manampiring", "Dalam buku ini, Henry Manampiring membahas filosofi Stoisisme yang mengajarkan pentingnya fokus pada hal-hal yang dapat kita kendalikan. Dengan menyajikan prinsip-prinsip seperti pengendalian emosi dan penerimaan terhadap keadaan, pembaca diajak untuk lebih rasional dan tenang dalam menghadapi tantangan hidup. Ditulis dengan gaya ringkas dan disertai contoh kehidupan sehari-hari, "Filosofi Teras" cocok untuk siapa saja yang ingin menjalani hidup lebih damai, bebas dari kecemasan berlebihan, dan lebih fokus pada hal-hal yang benar-benar penting.", "https://s3-ap-southeast-1.amazonaws.com/ebook-previews/45496/168985/1.jpg");
 
 document.addEventListener("DOMContentLoaded", function () {
     tampilkanKomentar();
